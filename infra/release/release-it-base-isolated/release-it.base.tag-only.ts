94ba278babf5c155c575802135db5d762f765c8f
Anton Stoychev <anton.stoychev@yotpo.com>
2025-07-10 11:28:37 +0300

    feat: deployment rollback + individual project deployments now happen automatically only if project has conventional changelog-worthy commits and automatic deployments are not paused

---
+++ infra/release/release-it-base-isolated/release-it.base.tag-only.ts
@@ -0,0 +1,21 @@
+/**
+ * This file is imported & transpiled by `release-it`
+ */
+import type { ReleaseItConfig } from './release.types';
+import baseConfig from './release-it.base';
+export * from './release.types';
+
+const tagOnlyConfig: ReleaseItConfig = {
+  ...baseConfig,
+  // @ts-expect-error -- `false` valid value for `npm` without types
+  npm: false,
+  git: {
+    ...baseConfig.git,
+    commit: false,
+    push: false,
+    requireUpstream: false,
+  },
+  hooks: {},
+};
+
+export default tagOnlyConfig;
